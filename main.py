d995bc7852a7279a3954d4ada81edd1ff01715cb
João Batista dos Santos Júnior <joaojuniorsantos2008@gmail.com>
2021-01-04 19:30:01 -0300

    Adicionando o talk back
    Falta investigar o overflowed to OS

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
-# Our main file.
-'''
-import speech_recognition as sr
-
-# Cria um reconhecedor
-r = sr.Recognizer()
-
-# Abrir o microfone para captura
-with sr.Microphone() as source:
-  while True:
-    audio = r.listen(source) #Define como fonte de audio
-    print(r.recognize_google(audio, language='pt-br'))
-'''
 #!/usr/bin/env python3
 
 from vosk import Model, KaldiRecognizer
 import os
+import pyaudio
+import pyttsx3
+import json
+
+# Síntese de fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
 
 if not os.path.exists('model'):
-    print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-    exit (1)
+    print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+    exit(1)
 
-import pyaudio
 
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1,
+                rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(2000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        result = rec.Result()
+        result = json.loads(result)
 
-print(rec.FinalResult())
+        if result is not None:
+            text = result['text']
+
+            print(text)
+            speak(text)
